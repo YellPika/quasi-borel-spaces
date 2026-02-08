8d76a0de3184047e1ae90c48280c1e24351a88ce
YellPika <yellpika@gmail.com>
2026-02-08 16:24:20 -0500

    feat(Sigma): some simplifications

--- QuasiBorelSpaces/OmegaCompletePartialOrder/Chain/Sigma.lean
+++ QuasiBorelSpaces/OmegaCompletePartialOrder/Chain/Sigma.lean
@@ -21,11 +21,6 @@ def inj {i} (c : Chain (P i)) : Chain ((i : I) × P i) where
 @[simp]
 lemma inj_coe {i} (c : Chain (P i)) (n : ℕ) : inj c n = ⟨i, c n⟩ := rfl
 
-private lemma cast_mono {i j : I} (h : i = j) : Monotone (cast (congr_arg P h)) := by
-  intro _ _ h'
-  subst h
-  exact h'
-
 /-- Converts a chain of coproducts into a coproduct of chains. -/
 def distrib (c : Chain ((i : I) × P i)) : (i : I) × Chain (P i) where
   fst := (c 0).fst
@@ -37,11 +32,14 @@ def distrib (c : Chain ((i : I) × P i)) : (i : I) × Chain (P i) where
         exact this.choose
       this ▸ (c n).snd
     monotone' i₁ i₂ hi := by
-      have h₀ := c.monotone (zero_le i₂)
       have h₁ := c.monotone hi
-      simp only [Sigma.le_def, eqRec_eq_cast] at h₀ h₁
-      simpa only [eqRec_eq_cast, ge_iff_le, cast_cast]
-        using cast_mono h₀.fst.symm h₁.snd
+      dsimp only [Lean.Elab.WF.paramLet]
+      generalize_proofs h₂
+      generalize c i₁ = ci₁, c i₂ = ci₂ at *
+      rcases h₁ with ⟨i, _, _, h₁⟩
+      dsimp at h₂
+      subst h₂
+      exact h₁
   }
 
 @[simp]
--- QuasiBorelSpaces/OmegaCompletePartialOrder/Sigma.lean
+++ QuasiBorelSpaces/OmegaCompletePartialOrder/Sigma.lean
@@ -13,25 +13,19 @@ variable {I : Type*} {P : I → Type*} [∀ i, OmegaCompletePartialOrder (P i)]
 instance : OmegaCompletePartialOrder ((i : I) × P i) where
   ωSup c := (Chain.Sigma.distrib c).map id fun _ ↦ ωSup
   le_ωSup c i := by
-    simp only [ge_iff_le]
-    cases c using Chain.Sigma.distrib_cases with
-    | mk c =>
-      simp only [
-        Chain.Sigma.inj_coe, id_eq, Chain.Sigma.distrib_inj,
-        Sigma.map_mk, Sigma.mk_le_mk_iff]
-      apply le_ωSup
+    cases c using Chain.Sigma.distrib_cases
+    simp only [
+      Chain.Sigma.inj_coe, id_eq, Chain.Sigma.distrib_inj,
+      Sigma.map_mk, Sigma.mk_le_mk_iff, le_ωSup]
   ωSup_le := by
     rintro c ⟨x, y⟩ h
-    simp only [ge_iff_le]
-    cases c using Chain.Sigma.distrib_cases with
-    | mk c =>
-      simp only [Chain.Sigma.inj_coe, ge_iff_le, Sigma.le_def] at h
-      have h' := (h 0).choose
-      subst h'
-      simp only [
-        exists_const, id_eq, Chain.Sigma.distrib_inj, Sigma.map_mk,
-        Sigma.mk_le_mk_iff, ωSup_le_iff] at ⊢ h
-      exact h
+    cases c using Chain.Sigma.distrib_cases
+    simp only [Chain.Sigma.inj_coe, ge_iff_le] at h
+    cases h 0
+    simp only [Sigma.mk_le_mk_iff] at h
+    simp only [
+      Sigma.map, id_eq, Chain.Sigma.distrib_inj,
+      Sigma.mk_le_mk_iff, ωSup_le_iff, h, implies_true]
 
 @[simp]
 lemma ωSup_inj {i} (c : Chain (P i)) : ωSup (Chain.Sigma.inj c) = ⟨i, ωSup c⟩ := rfl
